15533cf76108c6eeb51f35fb9cd396fd0d17b28f
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2021-06-09 14:49:37 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            <h1>About TODO LIST</h1> 
+            <h3>About TODO LIST</h3> 
             <p>You can store your list of things that you want to get done or that needs to get done :)</p>
         </div>
     )
